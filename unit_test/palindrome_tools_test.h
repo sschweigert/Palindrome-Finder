dd815e89dd1dc75054a68228c2429b203e0f544c
Sebastian Schweigert <sebastian@bobsweep.com>
2017-04-22 14:47:47 -0700

    Added some useful + working unit tests

--- unit_test/palindrome_tools_test.h
+++ unit_test/palindrome_tools_test.h
@@ -2,13 +2,41 @@
 
 #include <palindrome_tools.h>
 
-class MyTestSuite : public CxxTest::TestSuite
+class PalindromeToolsTest : public CxxTest::TestSuite
 {
+
 	public:
 
-		void testAddition( void )
+		void testPalindromeIndentification(void)
 		{
 			TS_ASSERT(isPalindrome("racecar"));
 			TS_ASSERT(!isPalindrome("bacecar"));
+		}
+
+		void testForwardWordIncrementation(void)
+		{
+			auto incrementResult = incrementWord("a");
+			
+			TS_ASSERT(incrementResult);
+			TS_ASSERT_EQUALS(*incrementResult, "b");
+
+			incrementResult = incrementWord("sebastian");
+
+			TS_ASSERT(incrementResult);
+			TS_ASSERT_EQUALS(*incrementResult, "sebastiao");
+
+			incrementResult = incrementWord("pizazz");
+
+			TS_ASSERT(incrementResult);
+			TS_ASSERT_EQUALS(*incrementResult, "pizbaa");
+
+			incrementResult = incrementWord("azzzzz");
+
+			TS_ASSERT(incrementResult);
+			TS_ASSERT_EQUALS(*incrementResult, "baaaaa");
+
+			incrementResult = incrementWord("zzzzzzz");
+
+			TS_ASSERT(!incrementResult);
 		}
 };
