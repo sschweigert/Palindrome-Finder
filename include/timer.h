cf4e69aa2fe6565f563d0588bbffe76a34912fec
Sebastian Schweigert <sebastian@bobsweep.com>
2017-04-23 17:42:54 -0700

    Commenting timer.h

--- include/timer.h
+++ include/timer.h
@@ -3,15 +3,20 @@
 
 #include <chrono>
 
+//! \brief Wrapper for chrono stdlib that makes timing of execution easy.
 class Timer
 {
 
 	public:
 
+		//! \brief Create and start a timer
 		Timer();
 
+		//! \brief Reset a timer's start to begin timing a process
 		void start();
 
+		//! \brief Obtain the time elapsed since Timer was constructed or the
+		//! last time start() was called, whichever is less
 		float secondsElapsed() const;
 
 	private:
--- src/timer.cpp
+++ src/timer.cpp
@@ -13,6 +13,7 @@ void Timer::start()
 float Timer::secondsElapsed() const
 {
 	auto currentTime = std::chrono::high_resolution_clock::now();
-	return std::chrono::duration_cast<std::chrono::nanoseconds>(currentTime - time).count(); 
+	auto numNanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(currentTime - time).count();
+	return numNanoseconds / 1e9; 
 
 }
