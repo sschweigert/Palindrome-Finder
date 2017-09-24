421b84d6739a78eb168020aa47f6a2a5c5783586
Sebastian Schweigert <sebastian@bobsweep.com>
2017-09-24 15:40:31 -0700

    Simplified palindrome_calculation_test and added another test

--- unit_test/palindrome_calculation_test.h
+++ unit_test/palindrome_calculation_test.h
@@ -10,11 +10,23 @@ class PalindromeCalculationTest : public CxxTest::TestSuite
 
 	public:
 
+		void verifyPalindrome(const std::vector<std::string>& input, int length, const std::unordered_set<std::string>& expectedOutput)
+		{
+			std::vector<std::string> result = calculatePalindromes(input, length);
+
+			TS_ASSERT(expectedOutput.size() == result.size());
+
+			for (const auto& resultString : result)
+			{
+				bool matchesExpected = (expectedOutput.count(resultString) == 1);
+				TS_ASSERT(matchesExpected);
+			}
+		}
+
 		// Simple "sanity check" using single letter words and exploiting the combinatorial
 		// nature of palindromes
 		void testSingleLetterPermutations(void)
 		{
-	
 			std::vector<std::string> seedWords =
 			{
 				"a",
@@ -22,7 +34,7 @@ class PalindromeCalculationTest : public CxxTest::TestSuite
 				"c"
 			};
 
-			std::vector<std::string> result = calculatePalindromes(seedWords, 3);
+			const int palindromeLength = 3;
 
 			std::unordered_set<std::string> expectedResult =
 			{
@@ -39,13 +51,52 @@ class PalindromeCalculationTest : public CxxTest::TestSuite
 				"c c c"
 			};
 
-			TS_ASSERT(expectedResult.size() == result.size());
+			verifyPalindrome(seedWords, palindromeLength, expectedResult);
+		}
 
-			for (const auto& resultString : result)
+		// Make sure palindromes are found in the extreme case of a, aaa, z and zzz.
+		// These have caused problems before where they were skipped or caused hanging/crashes.
+		void testFirstAndLastCheck(void)
+		{
+			std::vector<std::string> seedWords =
 			{
-				bool matchesExpected = (expectedResult.count(resultString) == 1);
-				TS_ASSERT(matchesExpected);
-			}
+				"a",
+				"aaa",
+				"z",
+				"zzz"
+			};
+
+			const int palindromeLength = 3;
+
+			std::unordered_set<std::string> expectedResult =
+			{
+				"a a a",
+				"a aaa a",
+				"a z a",
+				"a zzz a",
+				"a a aaa",
+				"a aaa aaa",
+				"aaa a a",
+				"aaa aaa a",
+				"aaa a aaa",
+				"aaa aaa aaa",
+				"aaa z aaa",
+				"aaa zzz aaa",
+				"z a z",
+				"z aaa z",
+				"z z z",
+				"z zzz z",
+				"z z zzz",
+				"z zzz zzz",
+				"zzz z z",
+				"zzz zzz z",
+				"zzz a zzz",
+				"zzz aaa zzz",
+				"zzz z zzz",
+				"zzz zzz zzz"
+			};
+
+			verifyPalindrome(seedWords, palindromeLength, expectedResult);
 		}
 
 };
