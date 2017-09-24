403f9c80fd989199b54c12d616c0509e77537639
Sebastian Schweigert <sebastian@bobsweep.com>
2017-09-24 15:30:53 -0700

    Expanding palindrome_calculation_test

--- unit_test/palindrome_calculation_test.h
+++ unit_test/palindrome_calculation_test.h
@@ -10,18 +10,33 @@ class PalindromeCalculationTest : public CxxTest::TestSuite
 
 	public:
 
-		void testBasicPalindromeCalculation(void)
+		// Simple "sanity check" using single letter words and exploiting the combinatorial
+		// nature of palindromes
+		void testSingleLetterPermutations(void)
 		{
+	
 			std::vector<std::string> seedWords =
 			{
-				"a"
+				"a",
+				"b",
+				"c"
 			};
 
 			std::vector<std::string> result = calculatePalindromes(seedWords, 3);
 
 			std::unordered_set<std::string> expectedResult =
 			{
-				"a a a"
+				"a a a",
+				"a b a",
+				"a c a",
+
+				"b a b",
+				"b b b",
+				"b c b",
+
+				"c a c",
+				"c b c",
+				"c c c"
 			};
 
 			TS_ASSERT(expectedResult.size() == result.size());
