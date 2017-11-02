37a2d835c0ca33138c42eec0cba4831696860ae1
Sebastian Schweigert <sebastian@bobsweep.com>
2017-11-01 21:53:57 -0700

    Added ability to specify word types

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@
 
 int main(int argc, char** argv)
 {
+	/*
 	boost::optional<std::vector<std::string>> words = importGoogleWords();
 
 	if (!words)
@@ -38,6 +39,14 @@ int main(int argc, char** argv)
 	std::cout << "Found " << palindromes.size() << " words. Now saving. " << std::endl;
 
 	saveToHomeDir(palindromes, "generated_palindromes.txt");
+	*/
 
+	EnumMap<WordType, std::vector<std::string>> typedWords = importTypedWords();
+
+	std::vector<WordType> types = { WordType::Adjective, WordType::Adjective, WordType::Adjective, WordType::Noun };
+
+	std::vector<std::string> palindromes = findTypedPalindromes(types, typedWords);
+
+	saveToHomeDir(palindromes, "generated_palindromes.txt");
 	return 0;
 }
